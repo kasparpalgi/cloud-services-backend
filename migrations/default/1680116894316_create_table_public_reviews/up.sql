cad357e0d41410e1d8825ab9f53554d1ddeb6ac9
@kaspar <team@crewnew.com>
2023-03-29 20:09:47 +0100

    Add reviews table

---
+++ migrations/default/1680116894316_create_table_public_reviews/up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "public"."reviews" ("id" integer NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , UNIQUE ("id"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_reviews_updated_at"
+BEFORE UPDATE ON "public"."reviews"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_reviews_updated_at" ON "public"."reviews"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
